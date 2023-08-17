434811cf8d18bdb2f8e86d20ae79c6a5499cdb59
CODEZPC <100663870+CODEZPC@users.noreply.github.com>
2023-08-17 11:20:25 +0800

    Add files via upload

--- character.py
+++ character.py
@@ -1,34 +1,60 @@
+ec = {
+    "wolf": "狼人",
+    "k_wolf": "狼王",
+    "wk_wolf": "白狼王",
+    "h_wolf": "隐狼",
+    "s_wolf": "石像鬼",
+    "b_wolf": "狼美人",
+    "witch": "女巫",
+    "hunter": "猎人",
+    "prophet": "预言家",
+    "guard": "守卫",
+    "cupid": "丘比特",
+    "d_hunter": "猎魔人",
+    "bomber": "炸弹人",
+    "knight": "骑士",
+    "bear": "驯熊师",
+    "wild_child": "熊孩子",
+    "g_keeper": "守墓人",
+}
+
+
 class characher(object):
-    def __init__(self,job,camp,love,alive,die_cause):
+    def __init__(self, job, camp, love, alive, die_cause):
         self.job = job
         self.camp = camp
         self.love = love
         self.alive = alive
         self.die_cause = die_cause
+
     def __str__(self):
         if self.alive == 1:
-            return self.job + ":最终存活"
+            return ec[self.job] + ":最终存活"
         else:
-            return self.job + ":" + self.die_cause
+            return ec[self.job] + ":" + self.die_cause
+
+
 class c_witch(characher):
-    def __init__(self,save,kill):
+    def __init__(self, save, kill):
         self.save = save
         self.kill = kill
-wolf = characher("wolf",0,0,1,-1)
-k_wolf = characher("k_wolf",0,0,1,-1)
-wk_wolf = characher("wk_wolf",0,0,1,-1)
-h_wolf = characher("h_wolf",0,0,1,-1)
-s_wolf = characher("s_wolf",0,0,1,-1)
-b_wolf = characher("b_wolf",0,0,1,-1)
-witch = c_witch(1,1)
-witch.job,witch.camp,witch.love,witch.alive,witch.die_cause = "witch",1,0,1,-1
-hunter = characher("hunter",1,0,1,-1)
-prophet = characher("prophet",1,0,1,-1)
-guard = characher("guard",1,0,1,-1)
-cupid = characher("cupid",-1,0,1,-1)
-d_hunter = characher("d_hunter",1,0,1,-1)
-bomber = characher("bomber",1,0,1,-1)
-knight = characher("knight",1,0,1,-1)
-bear = characher("bear",1,0,1,-1)
-wild_child = characher("wild_child",-1,0,1,-1)
-g_keeper = characher("g_keeper",1,0,1,-1)
+
+
+wolf = characher("wolf", 0, 0, 1, -1)
+k_wolf = characher("k_wolf", 0, 0, 1, -1)
+wk_wolf = characher("wk_wolf", 0, 0, 1, -1)
+h_wolf = characher("h_wolf", 0, 0, 1, -1)
+s_wolf = characher("s_wolf", 0, 0, 1, -1)
+b_wolf = characher("b_wolf", 0, 0, 1, -1)
+witch = c_witch(1, 1)
+witch.job, witch.camp, witch.love, witch.alive, witch.die_cause = "witch", 1, 0, 1, -1
+hunter = characher("hunter", 1, 0, 1, -1)
+prophet = characher("prophet", 1, 0, 1, -1)
+guard = characher("guard", 1, 0, 1, -1)
+cupid = characher("cupid", -1, 0, 1, -1)
+d_hunter = characher("d_hunter", 1, 0, 1, -1)
+bomber = characher("bomber", 1, 0, 1, -1)
+knight = characher("knight", 1, 0, 1, -1)
+bear = characher("bear", 1, 0, 1, -1)
+wild_child = characher("wild_child", -1, 0, 1, -1)
+g_keeper = characher("g_keeper", 1, 0, 1, -1)
--- main.py
+++ main.py
@@ -1,176 +1,201 @@
 #!python3
-# import
-import math
-import os
-import random
-import sys
-import time
-import character as c
-import pr
+try:
+    # import
+    import math
+    import os
+    import random
+    import sys
+    import time
+    import character as c
+    import pr
 
-# launch
-os.system("cls")
-err = 0
-print("狼人杀 Pro (Terminal) 正在启动中......")
-for i in range(1, 101):
-    print("\r", end=" ")
-    if i == 100:
-        print(
-            "\033[0;32m{}% [".format(i),
-            "=" * (i // 4),
-            "\033[0m",
-            end="\033[0;32m]\033[0m",
-        )
-    else:
-        print("{}% \033[0;33m[".format(i), "=" * (i // 4), "\033[0m", end=" ")
-    sys.stdout.flush()
-    time.sleep(random.randint(1, 30) / 100)
-time.sleep(1)
-# get character numbers
-err = 1
-while err:
-    try:
-        os.system("cls")
-        cn = int(input("输入游戏人数 \033[0;34m(6-16)\033[0m :"))
-    except:
-        pr.Enterr()
-        time.sleep(1)
-        continue
-    else:
-        err = 0
-    if cn < 6 or cn > 16:
+    # launch
+    os.system("cls")
+    err = 0
+    print("狼人杀 Pro (Terminal) 正在启动中......")
+    for i in range(1, 101):
+        print("\r", end=" ")
+        if i == 100:
+            print(
+                "\033[0;32m{}% [".format(i),
+                "=" * (i // 4),
+                "\033[0m",
+                end="\033[0;32m]\033[0m",
+            )
+        else:
+            print("{}% \033[0;33m[".format(i), "=" * (i // 4), "\033[0m", end=" ")
+        sys.stdout.flush()
+        time.sleep(random.randint(1, 30) / 100)
+    time.sleep(1)
+    # get character numbers
+    err = 1
+    while err:
+        try:
+            os.system("cls")
+            cn = int(input("输入游戏人数 \033[0;34m(6-16)\033[0m :"))
+        except:
+            pr.Enterr()
+            time.sleep(1)
+            continue
+        else:
+            err = 0
+        if cn < 6 or cn > 16:
+            err = 1
+            pr.Enterr()
+            time.sleep(1)
+    # get character jobs
+    cl = {
+        "A": 2,
+        "B": 0,
+        "C": 0,
+        "D": 0,
+        "E": 0,
+        "F": 0,
+        "G": 1,
+        "H": 1,
+        "I": 1,
+        "J": 1,
+        "K": 0,
+        "L": 0,
+        "M": 0,
+        "N": 0,
+        "O": 0,
+        "P": 0,
+        "Q": 0,
+    }
+    cj = {
+        "A": c.wolf,
+        "B": c.k_wolf,
+        "C": c.wk_wolf,
+        "D": c.h_wolf,
+        "E": c.s_wolf,
+        "F": c.b_wolf,
+        "G": c.witch,
+        "H": c.hunter,
+        "I": c.prophet,
+        "J": c.guard,
+        "K": c.cupid,
+        "L": c.d_hunter,
+        "M": c.bomber,
+        "N": c.knight,
+        "O": c.bear,
+        "P": c.wild_child,
+        "Q": c.g_keeper,
+    }
+    # -wolf
+    wn = math.floor(cn / 3)
+    ecn = wn - 2
+    if ecn:
         err = 1
-        pr.Enterr()
-        time.sleep(1)
-# get character jobs
-cl = {
-    "A": 2,
-    "B": 0,
-    "C": 0,
-    "D": 0,
-    "E": 0,
-    "F": 0,
-    "G": 1,
-    "H": 1,
-    "I": 1,
-    "J": 1,
-    "K": 0,
-    "L": 0,
-    "M": 0,
-    "N": 0,
-    "O": 0,
-    "P": 0,
-    "Q": 0,
-}
-cj = {
-    "A": c.wolf,
-    "B": c.k_wolf,
-    "C": c.wk_wolf,
-    "D": c.h_wolf,
-    "E": c.s_wolf,
-    "F": c.b_wolf,
-    "G": c.witch,
-    "H": c.hunter,
-    "I": c.prophet,
-    "J": c.guard,
-    "K": c.cupid,
-    "L": c.d_hunter,
-    "M": c.bomber,
-    "N": c.knight,
-    "O": c.bear,
-    "P": c.wild_child,
-    "Q": c.g_keeper,
-}
-# -wolf
-wn = math.floor(cn / 3)
-ecn = wn - 2
-cl["A"] = ecn
-if ecn:
+        while err:
+            os.system("cls")
+            check = ["B", "C", "D", "E", "F"]
+            print("请选择\033[0;34m", ecn, "\033[0m个特殊狼选项:")
+            print("B 狼王  C 白狼王  D 隐狼  E 石像鬼  F 狼美人")
+            inp = input()
+            if len(inp) != ecn:
+                pr.Enterr()
+                time.sleep(1)
+            else:
+                for i in inp:
+                    if i not in check:
+                        pr.Enterr()
+                        time.sleep(1)
+                        break
+                    else:
+                        del check[check.index(i)]
+                else:
+                    err = 0
+                    for i in inp:
+                        cl[i] = 1
+    # -god
+    ecn = cn - wn - 4
+    if ecn and ecn < 7:
+        err = 1
+        while err:
+            os.system("cls")
+            check = ["K", "L", "M", "N", "O", "P", "Q"]
+            print("以下是已有的神职，你还需要再选择\033[0;34m", ecn, "\033[0m个:")
+            print("女巫 猎人 预言家 守卫")
+            print("请选择:")
+            print("K 丘比特  L 猎人  M 炸弹人  N 骑士\nO 驯熊师  P 熊孩子  Q 守墓人")
+            inp = input()
+            if len(inp) != ecn:
+                pr.Enterr()
+                time.sleep(1)
+            else:
+                for i in inp:
+                    if i not in check:
+                        pr.Enterr()
+                        time.sleep(1)
+                        break
+                    else:
+                        del check[check.index(i)]
+                else:
+                    err = 0
+                    for i in inp:
+                        cl[i] = 1
+    elif ecn == 7:
+        for i in "KLMNOPQ":
+            cl[i] = 1
+    # -give
+    os.system("cls")
+    check = []
+    print("分配职位......")
+    for i in "ABCDEFGHIJKLMNOPQ":
+        if cl[i] > 0:
+            for j in range(cl[i]):
+                check.append(i)
+    player = []
+    for i in range(len(check)):
+        j = random.choice(check)
+        player.append(j)
+        del check[check.index(j)]
+    for i in range(1, 101):
+        print("\r", end=" ")
+        if i == 100:
+            print(
+                "\033[0;32m{}% [".format(i),
+                "=" * (i // 4),
+                "\033[0m",
+                end="\033[0;32m]\033[0m",
+            )
+        else:
+            print("{}% \033[0;33m[".format(i), "=" * (i // 4), "\033[0m", end=" ")
+        sys.stdout.flush()
+        time.sleep(random.randint(1, 8) / 100)
+    time.sleep(1)
+    for i in "AABCDEFGHIJKLMNOPQ":
+        if i in player:
+            player[player.index(i)] = cj[i]
+    # config
     err = 1
     while err:
         os.system("cls")
-        check = ["B", "C", "D", "E", "F"]
-        print("请选择\033[0;34m", ecn, "\033[0m个特殊狼选项:")
-        print("B 狼王  C 白狼王  D 隐狼  E 石像鬼  F 狼美人")
-        inp = input()
-        if len(inp) != ecn:
+        try:
+            inp = int(input("选择游戏模式：1 明牌  2 暗牌 :"))
+        except:
             pr.Enterr()
             time.sleep(1)
         else:
-            for i in inp:
-                if i not in check:
-                    pr.Enterr()
-                    time.sleep(1)
-                    break
-                else:
-                    del check[check.index(i)]
-            else:
+            if inp == 1 or inp == 2:
+                mode = inp
                 err = 0
-                for i in inp:
-                    cl[i] = 1
-# -god
-ecn = cn - wn - 4
-if ecn and ecn < 7:
-    err = 1
-    while err:
-        os.system("cls")
-        check = ["K", "L", "M", "N", "O", "P", "Q"]
-        print("以下是已有的神职，你还需要再选择\033[0;34m", ecn, "\033[0m个:")
-        print("女巫 猎人 预言家 守卫")
-        print("请选择:")
-        print("K 丘比特  L 猎人  M 炸弹人  N 骑士\nO 驯熊师  P 熊孩子  Q 守墓人")
-        inp = input()
-        if len(inp) != ecn:
-            pr.Enterr()
-            time.sleep(1)
-        else:
-            for i in inp:
-                if i not in check:
-                    pr.Enterr()
-                    time.sleep(1)
-                    break
-                else:
-                    del check[check.index(i)]
             else:
-                err = 0
-                for i in inp:
-                    cl[i] = 1
-elif ecn == 7:
-    for i in "KLMNOPQ":
-        cl[i] = 1
-# -give
-os.system("cls")
-check = []
-print("分配职位......")
-for i in "ABCDEFGHIJKMNOPQ":
-    if cl[i] > 0:
-        for j in range(cl[i]):
-            check.append(i)
-player = []
-for i in range(len(check)):
-    j = random.choice(check)
-    player.append(j)
-    del check[check.index(j)]
-for i in range(1, 101):
-    print("\r", end=" ")
-    if i == 100:
-        print(
-            "\033[0;32m{}% [".format(i),
-            "=" * (i // 4),
-            "\033[0m",
-            end="\033[0;32m]\033[0m",
-        )
-    else:
-        print("{}% \033[0;33m[".format(i), "=" * (i // 4), "\033[0m", end=" ")
-    sys.stdout.flush()
-    time.sleep(random.randint(1, 8) / 100)
-time.sleep(1)
-for i in "AAABCDEFGHIJKLMNOPQ":
-    if i in player:
-        player[player.index(i)] = cj[i]
+                pr.Enterr()
+                time.sleep(1)
 
-os.system("cls")
-for i in player:
-    print(i)
 
-input('运行结束，按"Enter"退出')
+    os.system("cls")
+    for i in range(cn):
+        print(str(i + 1) + "号玩家:", player[i])
+
+    input('运行结束，按"Enter"退出')
+except:
+    os.system("cls")
+    print("\033[0;31mOops!\033[0m")
+    print("\033[0;31m): 程序运行出错\033[0m")
+    print("\033[0;31m可能是 输入错误 ，但是未被系统识别；\033[0m")
+    print("\033[0;31m也有可能是 程序意外错误 。\033[0m")
+    print("\033[0;31m请重启程序，或联系开发人员(zpcpy@outlook.com)\033[0m")
+    input('运行结束，按"Enter"退出')
