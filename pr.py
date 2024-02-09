5c27ebb5810070c2154e0221d187dd2f546a89ed
CODEZPC <100663870+CODEZPC@users.noreply.github.com>
2024-02-09 15:53:03 +0800

    Delete pr.py

--- pr.py
+++
@@ -1,8 +0,0 @@
-import random
-def Enterr():
-    print("\033[0;31m输入错误，请重试。\033[0m")
-def mx(n):
-    if n+75<10000:
-        return random.randint(0,75)
-    else:
-        return random.randint(0,10000-n)
